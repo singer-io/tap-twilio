be673aad102cd4c4d2c98b6b69013bb2605c9c85
Andy Lu <andy@stitchdata.com>
2023-05-30 20:26:25 +0000

    Bump version, update changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-twilio',
-      version='0.1.0',
+      version='0.1.1',
       description='Singer.io tap for extracting data from the Twilio API',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
